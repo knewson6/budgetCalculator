10e0cbb832caed7392ff9c0ff3679275fa7c2163
knewson <kylednewson@gmail.com>
2025-01-16 10:24:51 -0500

    update.1.16.25

--- budgetCalculator.py
+++ budgetCalculator.py
@@ -1,21 +1,5 @@
-import numpy as np
-import math
 from budgetCalculatorFunctions import getChar, getFloat, getExpenses, getIncome, getSavings, getTaxDeductibleSavings, federalTax, provincialTax
+from graphicUserInterface import graphicUI
 
+graphicUI()
 
-print('\nWelcome to our Canadian Budget Calculator')
-print('1) Start a new file')
-print('2) Update an old file')
-print('3) Delete an old file')
-print('4) Use the calculator without a file')
-print('5) Exit')
-
-userInput = input('\n\tHow can we assist? : ')
-
-if userInput == '4':
-    income = getIncome()
-    expenses = getExpenses()
-    taxDeductibles = getTaxDeductibleSavings()
-    savings = getSavings()
-    federalTax = federalTax(income, taxDeductibles)
-    taxedIncome = provincialTax(federalTax)
