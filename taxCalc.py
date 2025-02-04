179b903515261082b2df2f95953b6745aad31873
knewson <kylednewson@gmail.com>
2025-02-04 10:28:51 -0500

    update feb.4.2025.01

--- taxCalc.py
+++ taxCalc.py
@@ -17,48 +17,56 @@ def taxCalculator(root, mainScreen):
 
     monthlyIncomeLabel = tk.Label(root, text="Monthly Income \n\t\t $", font=('Times New Roman', 18), bg='white')
     monthlyIncomeLabel.place(x=75, y=250)
-    monthlyIncome = tk.Text(root, height=1, width=10, font=('Times New Roman', 18), highlightbackground='black', highlightcolor='black', highlightthickness='2')
+    monthlyIncome = tk.Entry(root, width=10, font=('Times New Roman', 18), highlightbackground='black', highlightcolor='black', highlightthickness='2')
     monthlyIncome.place(x=300, y=275)
 
     rrspContributionLabel = tk.Label(root, text="RRSP Contribution \n\t\t $", font=('Times New Roman', 18), bg='white')
     rrspContributionLabel.place(x=75, y=325)
-    rrspContribution = tk.Text(root, height=1, width=10, font=('Times New Roman', 18), highlightbackground='black', highlightcolor='black', highlightthickness='2')
+    rrspContribution = tk.Entry(root, width=10, font=('Times New Roman', 18), highlightbackground='black', highlightcolor='black', highlightthickness='2')
     rrspContribution.place(x=300, y=350)
 
     fhsaContributionLabel = tk.Label(root, text="FHSA Contribution \n\t\t $", font=('Times New Roman', 18), bg='white')
     fhsaContributionLabel.place(x=75, y=400)
-    fhsaContribution = tk.Text(root, height=1, width=10, font=('Times New Roman', 18), highlightbackground='black', highlightcolor='black', highlightthickness='2')
+    fhsaContribution = tk.Entry(root, width=10, font=('Times New Roman', 18), highlightbackground='black', highlightcolor='black', highlightthickness='2')
     fhsaContribution.place(x=300, y=425)
 
     capitolGainsLabel = tk.Label(root, text="Realised Capitol Gains \n\t\t $", font=('Times New Roman', 18), bg='white')
     capitolGainsLabel.place(x=75, y=475)
-    capitolGains = tk.Text(root, height=1, width=10, font=('Times New Roman', 18), highlightbackground='black', highlightcolor='black', highlightthickness='2')
+    capitolGains = tk.Entry(root, width=10, font=('Times New Roman', 18), highlightbackground='black', highlightcolor='black', highlightthickness='2')
     capitolGains.place(x=300, y=500)
+
+    calculateButton = tk.Button(root, text="Calculate!", font=('Times New Roman', 12), command=lambda: getUserInput())
+    calculateButton.place(x=800, y=600, height=50, width=100)
 
     backButton = tk.Button(root, text="Back", font=('Times New Roman', 12), command=lambda: mainScreen(root))
     backButton.place(x=1050, y=600, height=50, width=100)
 
     def getUserInput():
         userProvince = provinces.get()
-        userMonthlyIncome = monthlyIncome.get("1.0", END).strip()
-        userRRSPcontribution = rrspContribution.get("1.0", END).strip()
-        userFHSAcontribution = fhsaContribution.get("1.0", END).strip()
-        userCapitolGains = capitolGains.get("1.0", END).strip()
+        userMonthlyIncome = monthlyIncome.get().strip()
+        userRRSPcontribution = rrspContribution.get().strip()
+        userFHSAcontribution = fhsaContribution.get().strip()
+        userCapitolGains = capitolGains.get().strip()
 
-        return userProvince, userMonthlyIncome, userRRSPcontribution, userFHSAcontribution, userCapitolGains
+        # check user only inputted numbers
+        # create a popup error message to let them know to only enter numbers
+
+        return userProvince, userMonthlyIncome , userRRSPcontribution, userFHSAcontribution, userCapitolGains
 
 
 def provincialTax():
 
-def federalTax(provincialTax):
 
-    if provincialTax <= 55867:
-        taxedIncome = provincialTax * 0.85
-    elif 55867 < provincialTax <= 111733:
-        taxedIncome = (55867 * 0.85) + ((provincialTax - 55867) * 0.795)
-    elif 111733 < provincialTax <= 173205:
-        taxedIncome = (55867 * 0.85) + ((provincialTax - 55867) * 0.795) + ((provincialTax - 111733) * 0.74)
-    elif 173205 < provincialTax <= 256752:
-        taxedIncome = (55867 * 0.85) + ((provincialTax - 55867) * 0.795) + ((provincialTax - 111733) * 0.74) + ((provincialTax - 256752) * 0.71)
-    elif provincialTax > 256752:
-        taxedIncome = (55867 * 0.85) + ((provincialTax - 55867) * 0.795) + ((provincialTax - 111733) * 0.74) + ((provincialTax - 256752) * 0.71) + ((provincialTax - 256752) * 0.67)
+
+# def federalTax(provincialTax):
+#
+#    if provincialTax <= 55867:
+#        taxedIncome = provincialTax * 0.85
+#    elif 55867 < provincialTax <= 111733:
+#        taxedIncome = (55867 * 0.85) + ((provincialTax - 55867) * 0.795)
+#    elif 111733 < provincialTax <= 173205:
+#        taxedIncome = (55867 * 0.85) + ((provincialTax - 55867) * 0.795) + ((provincialTax - 111733) * 0.74)
+#    elif 173205 < provincialTax <= 256752:
+#        taxedIncome = (55867 * 0.85) + ((provincialTax - 55867) * 0.795) + ((provincialTax - 111733) * 0.74) + ((provincialTax - 256752) * 0.71)
+#    elif provincialTax > 256752:
+#        taxedIncome = (55867 * 0.85) + ((provincialTax - 55867) * 0.795) + ((provincialTax - 111733) * 0.74) + ((provincialTax - 256752) * 0.71) + ((provincialTax - 256752) * 0.67)
